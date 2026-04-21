9028ac15cddb69e1c7a00c24a33c9b16c4993d8a
kristenYu <kkyu@ualberta.ca>
2026-04-21 16:28:19 -0700

    glitching bl core

--- Assets/scripts/StoryManager.cs
+++ Assets/scripts/StoryManager.cs
@@ -1,5 +1,6 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoryManager : MonoBehaviour
 {
@@ -15,9 +16,27 @@ public class StoryManager : MonoBehaviour
     public GameObject spaceContinue;
 
     public GameObject coreBl;
+    public Image coreBlSpriteRenderer;
+
     public GameObject coreTl;
+    public Image coreTlSpriteRenderer;
     public GameObject coreTr;
+
+    public Image coreTrSpriteRenderer;
     public GameObject coreBr;
+    public Image coreBrSpriteRenderer;
+
+    public Sprite[] coreBlSprites;
+    public Sprite[] coreTlSprites;
+    public Sprite[] coreTrSprites;
+    public Sprite[] coreBrSprites;
+
+    public float glitchLengthTimer; 
+    public float randomGlitchLength;
+    public float startGlitchTimer;
+    public float randomStartGlitch; 
+
+    public bool isCoreB1Glitching;
 
 
     public bool isIntro;
@@ -37,10 +56,26 @@ public class StoryManager : MonoBehaviour
         coreTl.SetActive(false);
         coreTr.SetActive(false);
 
+        startGlitchTimer = 0;
+        randomStartGlitch = Random.Range(0f, 5f);
+        isCoreB1Glitching = false;
     }
 
     void Update()
     {
+        glitchLengthTimer += Time.deltaTime;
+        startGlitchTimer += Time.deltaTime;
+        if(isCoreB1Glitching)
+        {
+            if(glitchLengthTimer > randomGlitchLength)
+            {
+                resetCore(coreBlSpriteRenderer, coreBlSprites);
+                startGlitchTimer = 0;
+                isCoreB1Glitching = false;
+            }
+        }
+       
+
         if(isIntro)
         {
             if(Input.GetKeyDown(KeyCode.Space))
@@ -53,12 +88,24 @@ public class StoryManager : MonoBehaviour
                 isIntro = false;
             }
         }
+        if(coreBl.activeSelf)
+        {
+            if(!isCoreB1Glitching)
+            {
+                if(startGlitchTimer > randomStartGlitch)
+                {
+                    glitchCore(0, coreBlSpriteRenderer, coreBlSprites);
+                    isCoreB1Glitching = true;
+                }
+            }
+        }   
     }
 
     public void SetDecision(TalkingState talkingState)
     {
         decisionArray[currentDecision] = talkingState;
         frameManager.currentTalkingState = TalkingState.player;
+        startGlitchTimer = 0;
         if(currentDecision < decisionArray.Length)
         {
             currentDecision += 1;
@@ -81,4 +128,15 @@ public class StoryManager : MonoBehaviour
         }
     }
 
+    public void glitchCore(int decisionPoint, Image coreRenderer, Sprite[] spriteArray)
+    {   
+        glitchLengthTimer = 0;
+        randomGlitchLength = Random.Range(0f, 1f);
+        coreRenderer.sprite = spriteArray[(int)decisionArray[decisionPoint]];
+    }
+
+    public void resetCore(Image coreRenderer, Sprite[] spriteArray)
+    {
+        coreRenderer.sprite = spriteArray[0]; //hard coded to be 0 so it is the core without the glitch
+    }
 }
